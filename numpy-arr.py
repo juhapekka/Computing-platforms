767fead3ac29b28f121e7083fde9e0637278e640
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2025-02-07 22:30:02 +0200

    jotain muutoksia

--- numpy-arr.py
+++ numpy-arr.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-# Create a 7000 x 7000 NumPy array
 cols = 7000
 rows = 7000
 array = np.arange(cols, dtype=np.int64).repeat(rows).reshape(rows, cols)
 
-# Compute the sum of all elements (column-wise traversal)
-total_sum = np.sum(array.T)  # Transposing ensures column-wise traversal
+summa = sum(np.sum(array, axis=0))
 
-print(total_sum)
+print(summa)
